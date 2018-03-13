d65cc039b8efe11e994d3fa19c06330399172d40
dimortizp <dimortizp91@gmail.com>
2018-03-12 20:52:25 -0500

    CRUD para usuarios e inicio de administración de CLAIMS

---
+++ src/PYS.IdentityServer.Security.Administration/Authorize/Claims/ClaimsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using IdentityServerWithAspIdAndEF.Models;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using IdentityServerWithAspIdAndEF.Services;
+using System.Security.Claims;
+
+namespace PYS.IdentityServer.Security.Administration.Authorize.Claims
+{
+    public class ClaimsController : Controller
+    {
+
+        #region private variables
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IIdentityServerInteractionService _interaction;
+        private readonly IClientStore _clientStore;
+        private readonly IAuthenticationSchemeProvider _schemeProvider;
+        private readonly IEventService _events;
+        private readonly ILogger _logger;
+        private readonly IEmailSender _emailSender;
+
+        #endregion
+
+        #region contructors
+
+        public ClaimsController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IIdentityServerInteractionService interaction,
+            IClientStore clientStore,
+            IAuthenticationSchemeProvider schemeProvider,
+            IEventService events,
+            IEmailSender emailSender,
+            ILogger<ClaimsController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _interaction = interaction;
+            _clientStore = clientStore;
+            _schemeProvider = schemeProvider;
+            _events = events;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+
+        #endregion
+
+        #region CRUD
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        #endregion
+
+        #region Extended Functionalities
+
+        public async Task<IActionResult> GetUserClaims(string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            List<Claim> res = (await _userManager.GetClaimsAsync(user)).ToList();
+
+            ClaimsViewModel vm = new ClaimsViewModel() { User = user, Claims = res };
+            return View(vm);
+        }
+
+        public async Task<IActionResult> AddAplicationToUser(string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+
+            return View();
+
+        }
+
+
+        #endregion
+    }
+}
---
+++ src/PYS.IdentityServer.Security.Administration/Authorize/Users/UsersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServerWithAspIdAndEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PYS.IdentityServer.Security.Administration.Authorize.Users
+{
+    public class UsersViewModel : Pagination
+    {
+        public List<ApplicationUser> Users { get; set; }
+
+    }
+}
